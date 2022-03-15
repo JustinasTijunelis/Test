34cab80d4bdf732b8f2d02f3fbb6880e56d11fc2
JustinasTijunelis <justinas.tijunelis@gmail.com>
2022-03-15 19:38:49 +0100

    Pirma pamoka

---
+++ 1 Pamoka/Program.cs
@@ -0,0 +1,135 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Pirma_pamoka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 uzduotis
+
+            //string vardas = ""; //String yra ivesties duomenu rezervavimas
+            //string spalva = "";
+
+            //while (spalva != "Stop") //Veikia kaip IF
+            //{
+            //    Console.WriteLine("Labas mano vardas Justinas, o koks tavo"); //C isvedimas teksto, 
+            //    vardas = Console.ReadLine();
+            //    //Ivedimas ir automatiskai issaugo musu paruostame ivesties duomenu
+            //    Console.WriteLine($"Labas { vardas} kokia tavo megstamiausia spalva");
+            //    spalva = Console.ReadLine();
+
+            //    Console.WriteLine($"Labas { vardas} mano taip pat - { spalva} ");
+            //}
+            //Console.Clear();  //isvalo consoles duomenis 
+            //Console.WriteLine("Good bey"); //isvedimas 
+            //Console.ReadLine();
+
+            //2 uzduotis
+
+            //int skaicius1 = 17;
+            //int skaicius2 = 23;
+            //int suma = skaicius1 + skaicius2;
+
+            // Console.WriteLine($" Skaiciu suma { skaicius1} + { skaicius2} = { suma}");
+
+
+            //3 uzduotis
+
+            //double skaicius1 = 24;   //int naudojamas tik tikslieji be kablelio skaiciai 
+            //double skaicius2 = 5;   //double naudojamas su skaiciais po kablelio
+            //double suma = skaicius1 / skaicius2;
+
+            //Console.WriteLine($" Skaiciu suma { skaicius1} / { skaicius2} = { suma}");
+
+
+            //4 uzduotis
+
+            //int uzduotis1 = -6 + 3 * 5;
+            //int uzduotis2 = (13 - 2) * 7;
+            //int uzduotis3 = (5 + -2) + (20 / 10);
+
+            //Console.WriteLine($" { uzduotis1} ");
+            //Console.WriteLine($" { uzduotis2} ");
+            //Console.WriteLine($" { uzduotis3} ");
+
+            //5 uzduotis
+
+            //Console.WriteLine($"ivestikite pirma skaiciu");
+            //string input1 =  Console.ReadLine();
+            //int number1 = int.Parse(input1); //String convertina i Int (skaicius) 
+
+
+            //Console.WriteLine($"ivestikite antra skaiciu");
+            //string input2 = Console.ReadLine();
+            //int number2 = int.Parse(input2);
+
+            //Console.WriteLine($"{number1}, {number2} suma: {number1 + number2}");
+            //Console.WriteLine($"{number1}, {number2} skirtumas: {number1 - number2}");
+            //Console.WriteLine($"{number1}, {number2} dalyba: {number1 / number2}");
+            //Console.WriteLine($"{number1}, {number2} daugyba: {number1 * number2}");
+
+
+            //6 uzduotis
+
+            //Console.WriteLine($"Iveskite skaiciu");
+            //string input1 = Console.ReadLine();
+            //int num = int.Parse(input1);
+            //int A1 = num * 1;
+            //int A2 = num * 2;
+            //int A3 = num * 3;
+            //int A4 = num * 4;
+            //int A5 = num * 5;
+            //int A6 = num * 6;
+            //int A7 = num * 7;
+            //int A8 = num * 8;
+            //int A9 = num * 9;
+            //int A10 = num * 10;
+
+            //Console.WriteLine($" { num} * 1 = { A1}");
+            //Console.WriteLine($" { num} * 2 = { A2}");
+            //Console.WriteLine($" { num} * 3 = { A3}");
+            //Console.WriteLine($" { num} * 4 = { A4}");
+            //Console.WriteLine($" { num} * 5 = { A5}");
+            //Console.WriteLine($" { num} * 6 = { A6}");
+            //Console.WriteLine($" { num} * 7 = { A7}");
+            //Console.WriteLine($" { num} * 8 = { A8}");
+            //Console.WriteLine($" { num} * 9 = { A9}");
+            //Console.WriteLine($" { num} * 10 = { A10}");
+
+
+            //7 uzduotis
+
+            //int number1, number2, number3, avarage;
+
+            //Console.WriteLine("Iveskite 1 sk.");
+            //number1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Iveskite 2 sk.");
+            //number2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Iveskite 3 sk.");
+            //number3 = Convert.ToInt32(Console.ReadLine());
+
+            //avarage = ( number1 +  number2 + number3) / 3;
+            //Console.WriteLine($"Vidurkis { avarage}");
+
+
+            //8 uzduotis
+
+
+
+            //string name = ""; //String yra ivesties duomenu rezervavimas
+
+            //while(name != "Stop") //Veikia kaip IF
+            //{
+            //    Console.WriteLine("Enter your name"); //C isvedimas teksto, 
+            //    name =  Console.ReadLine(); //Ivedimas ir automatiskai issaugo musu paruostame ivesties duomenu
+
+            //    Console.WriteLine($"Hello { name}");
+            //}
+            //Console.Clear();  //isvalo consoles duomenis 
+            //Console.WriteLine("Good bey"); //isvedimas 
+            //Console.ReadLine();
+
+        }
+    }
+}
