c0abd461579693080c22a39ec1d06215ba698ac8
JustinasTijunelis <justinas.tijunelis@gmail.com>
2022-03-17 18:31:12 +0100

    4 paskaita

---
+++ 4 Cilkai/Program.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Pirma_Ciklai
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Didejanti ciklas
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"Sukasi ciklas siuo metu {i}");
+            //}
+            ////Mazejantis cikas
+            //for (int i = 10; i >= 0 ; i--)
+            //{
+            //    Console.WriteLine($"Sukasi ciklas siuo metu {i}");
+
+            //}
+
+            //Cikas su zingsniu 
+            //for (int i = 0; i < 10; i+=2) //visada prides 2 
+            //{
+            //    Console.WriteLine($"Sukasi ciklas siuo metu {i}");
+            //}
+            ////Interacija per diapazona 
+            //for (char i = 'a'; i = 'z'; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+                        //iteracija per stringus
+            //string name = "Petriukas";
+            //int lenght = name.Length;
+
+            //for (int i = 0; i < name.length; i++)
+            //{
+            //    char current = name[i];
+            //    Console.WriteLine(current);
+            //}
+
+                        ////Pabegimas is ciklo 
+            //for (int i = 0; i < name.length; i++)
+            //{
+            //    char current = name[i];
+            //    if (current =='u')
+            //    {
+            //        break;  //galima naudoti "continue" tam kad grazintu kaip i paieska
+            //    }
+            //    Console.WriteLine(current);
+            //}
+
+            // 1 Uzduotis
+            Console.WriteLine(" Prasau iveskite bet koki skaiciu");
+            string sk = Console.ReadLine();
+            int input = int.Parse(sk);
+            Console.Clear();
+
+            int suma = input + i;
+            for (int i = 1; i < input; i++)
+
+
+            {
+                int suma = input + i;
+                Console.WriteLine(suma);
+
+            }
+
+        }
+    }
+}
